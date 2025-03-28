9a24a67579b9c699ea4fb5001ba70140a5191019
elleon7771 <mahatirsamm@gmail.com>
2025-03-28 23:55:31 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I have crush on you').split('')
+    const titles = ('I have crush on you valen').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
